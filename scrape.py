ba1e864998a3d4ff907efd089c40a9e4441db20b
Dan Salmon <salmon@protonmail.ch>
2017-12-14 20:48:02 -0600

    Quick clean and add license

--- scrape.py
+++ scrape.py
@@ -1,23 +1,25 @@
-###################################################
-# fccedits
+#!/usr/bin/env python
+
+#############
 #
-# Scrape Wikipedia for edits made by FCC IP's
+# fccedits - Scrape Wikipedia for edits made by FCC IP's
 #
 # Author: @bltjetpack, github.com/sa7mon
 # Created: 12/14/17
+# License: Creative Commons (CC BY-NC-SA 4.0))
 #
-####################################################
+#############
 
 from netaddr import *
 import requests
 from bs4 import BeautifulSoup
+
 
 ipset = IPSet()
 
 ipset.add('192.104.54.0/24') # s/o to @Viss for finding
 
 for ip in ipset:
-	# https://en.wikipedia.org/wiki/Special:Contributions/119.18.13.17
 	print("Checking ip: " + str(ip) + "...", end='', flush=True)
 
 	editsPage = BeautifulSoup(requests.get('https://en.wikipedia.org/wiki/Special:Contributions/' + str(ip)).text, "html.parser")
